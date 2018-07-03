e83b39ac193efb5b0d6ff94556a7f39cb2de563c
綾木健一郎 <kenichiro-ayaki@live.jp>
2018-07-04 01:00:21 +0900

    Relaaseモード/Debugモードでのフォルダ設定を変更

--- PerformanceTest/My Project/Settings.Designer.vb
+++ PerformanceTest/My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -56,7 +56,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("..\src\PerformanceTest\kokoro.txt")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("..\..\src\PerformanceTest\kokoro.txt")>  _
         Public Property TargetFile() As String
             Get
                 Return CType(Me("TargetFile"),String)
